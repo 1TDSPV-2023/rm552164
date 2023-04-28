14b691c75a2cf817493145ab5e794f9ea7ec5e85
CarlosEduardo7700 <rm552164@fiap.com.br>
2023-04-27 22:53:26 -0300

    If Ternário + JavaScript.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-alert("Alô Mundo!!")
+// let nr1 = 10
+// let nr2 = 5
+
+// let resultado = !(nr1 % nr2) ? "PAR" : "IMPAR"
+
+// console.log(resultado)
+
+const menu = document.getElementById("menu")
+
+console.log(menu.textContent)
