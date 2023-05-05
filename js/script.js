4c6cc5eed5bbf9421ab38f08f797dc2ebd11c42b
CarlosEduardo7700 <rm552164@fiap.com.br>
2023-05-04 22:16:44 -0300

    Array e seus métodos.

--- js/script.js
+++ js/script.js
@@ -5,6 +5,50 @@
 
 // console.log(resultado)
 
-const menu = document.getElementById("menu")
+// const menu = document.getElementById("menu")
 
-console.log(menu.textContent)
+// console.log(menu.textContent)
+
+
+
+
+// let frutas = ["laranja", "banana", "acerola", "abacate", "caqui", "uva"]
+
+// // adiciona no fianl
+// frutas.push("kiwi")
+// // adiciona no inicio
+// frutas.unshift("melancia")
+// // remover no fianl
+// frutas.pop()
+// // remover no inicio
+// frutas.shift()
+
+// // buscar elementos
+// let fruta = frutas.indexOf("abacate")
+// // remover um item específico
+// frutas.splice(fruta, 1) 
+
+
+
+
+let frutas = ["laranja", "banana", "acerola", "abacate", "caqui", "uva"]
+
+// For
+for(let x = 0; x < frutas.length; x++){
+    console.log(x)
+}
+// For of
+for (const fruta of frutas){
+    console.log(fruta)
+}
+// For in
+for(const indice in frutas){
+    console.log(frutas[indice])
+}
+// Map
+frutas.map((fruta, key)=>{
+    console.log(`Fruta nr${key + 1} ${fruta}`);
+});
+
+
+
