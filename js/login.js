7bde6fa1a0dc6f88b676aa0b54b198f1f650629e
CarlosEduardo7700 <rm552164@fiap.com.br>
2023-05-16 23:00:22 -0300

    Javascript: validação do Login e função do Olho.

--- js/login.js
+++ js/login.js
@@ -1,24 +1,32 @@
 "use strict";
 
-// Criando objetos:
+let listaDeUsuarios = [
+    {
+        nomeCompleto : "José das Couves",
+        nomeUsuario : "jose",
+        senhaUsuario : "123456"
+    }
+    ,
+    {
+        nomeCompleto : "João Paulino",
+        nomeUsuario : "joao",
+        senhaUsuario : "123456"
+    }
+    ,
+    {
+        nomeCompleto : "Maria Tomaite",
+        nomeUsuario : "maria",
+        senhaUsuario : "123456"
+    }
+    ,
+    {
+        nomeCompleto : "Paulo da Selva",
+        nomeUsuario : "paulo",
+        senhaUsuario : "123456"
+    }
+];
 
-const usuario1 = {
-    nomeUsuario : "rita-lee",
-    senhaUsuario : "00000"
-}
-
-const usuario2 = {
-    nomeUsuario : "ptt01",
-    senhaUsuario : "12345"
-}
-
-usuario2.nomeUsuario = "Juquinha"
-
-// lista de usuarios
-
-let listaDeUsuarios = []
-listaDeUsuarios.push(usuario1)
-listaDeUsuarios.push(usuario2)
+localStorage.setItem("listaUser", JSON.stringify(listaDeUsuarios));
 
 addEventListener("click", (evento)=>{
     // console.log(evento.target);
@@ -26,10 +34,28 @@ addEventListener("click", (evento)=>{
     let userInput = document.querySelector("#idUser");
     let passInput = document.querySelector("#idPass");
 
+    let eyePass = document.querySelector("i")
+
+    //MOSTRAR SENHA
+
+    if(evento.target.className == "fa fa-eye" || evento.target.className == "fa fa-eye-slash"){ 
+        evento.target.setAttribute("style", "cursor: pointer")
+        if(passInput.getAttribute("type") == "password"){
+            passInput.setAttribute("type", "text");
+            eyePass.setAttribute("class", "fa fa-eye-slash")
+        }
+        else{
+            passInput.setAttribute("type", "password");
+            eyePass.setAttribute("class", "fa fa-eye")
+        }
+    }
+
+    let listaDeUsuariosRecuperada = JSON.parse(localStorage.getItem("listaUser"))
+
     if(evento.target.id == "btnSubmit"){
 
     try{
-            listaDeUsuarios.forEach((usuario)=>{
+            listaDeUsuariosRecuperada.forEach((usuario)=>{
 
             if(userInput.value == usuario.nomeUsuario && passInput.value == usuario.senhaUsuario){
                 throw "Usuário Validado!"
@@ -40,7 +66,8 @@ addEventListener("click", (evento)=>{
 
     }catch(err){
         if(err == "Usuário Validado!"){
-            console.log("Usuário Validado!");
+            // Redirect
+            window.location.href = "../sucesso.html"
         }else{
             console.log("Usuário ou Senha inválido!");
         }
